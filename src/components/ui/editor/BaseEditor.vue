c444ec77e1b54bc846fd1fcc83723f26f08c6e5a
ardans <ardans@foxmail.com>
2024-03-25 17:21:03 +0800

    feat: init editor

---
+++ src/components/ui/editor/BaseEditor.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [
+    StarterKit,
+  ],
+  autofocus: true,
+})
+
+onUnmounted(() => {
+  editor.value.destroy()
+})
+</script>
+
+<template>
+  <div>
+    <EditorContent :editor="editor" />
+  </div>
+</template>
